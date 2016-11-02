ef2ef09ad48784a1c83dc2b894a7edf6e4abc595
Nazar32 <nazar3223@gmail.com>
2016-11-02 20:11:25 +0200

    created nested component

---
+++ src/app/containers/main.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'main-container',
+    templateUrl: 'app/containers/main.html'
+})
+
+export class Main{
+
+};
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,18 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
-import { App } from './app/app';
+import { App } from './app';
+import { Main } from './app/containers';
 
 @NgModule({
-    declarations: [App],
+    declarations: [
+        App,
+        Main
+    ],
     imports: [BrowserModule],
     bootstrap: [App]
 })
+
 export class AppModule {
 
 };
