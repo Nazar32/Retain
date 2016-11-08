df8d5035d763fb5feafada7d87f5cb9339c676d0
Nazar32 <nazar3223@gmail.com>
2016-11-08 17:35:22 +0200

    added note service

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -1,1 +1,60 @@
-export class ApiService {}
+import { Injectable } from '@angular/core';
+import { Headers, Response, Http } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjx/Rx';
+import 'rxjs/add/observable/throw';
+
+@Injectable()
+export class ApiService {
+    headers: Headers = new Headers({
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    });
+
+    api_url: string = 'http://localhost:3500';
+
+    constructor(private http: Http) {
+
+    }
+
+    private getJson(response: Response) {
+        return response.json;
+    }
+
+    private checkForError(response: Response): Response {
+        if (response.status >= 200 && response.status < 300) {
+            return response;
+        }
+        else {
+            const error = new Error(response.statusText);
+            error['response'] = response;
+            console.log(error);
+            throw error;
+        }
+    }
+
+    get(path:string): Observable<any> {
+        return this.http.get('${this.api_url}${path}', this.headers)
+        .map(this.checkForError)
+        .catch(err => Observable.throw(err))
+        .map(this.getJson)
+    }
+
+    post(path: string, body): Observable<any> {
+        return this.http.post(
+            '${this.api_url}${path}',
+            JSON.stringify(body),
+            {headers: this.headers}
+        )
+        .map(this.checkForError)
+        .catch(err => Observable.throw(err))
+        .map(this.getJson)
+    }
+
+    delete(path:string): Observable<any> {
+        return this.http.delete('${this.api_url}${path}', this.headers)
+        .map(this.checkForError)
+        .catch(err => Observable.throw(err))
+        .map(this.getJson)
+    }
+}
---
+++ src/app/services/notes.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core'; 
+import { ApiService } from './api';
+
+export class NoteService {
+    path: string = '/notes';
+    constructor(private api: ApiService) {
+
+    }
+
+    createNote(note) {
+        return this.api.post(this.path, note);
+    }
+
+    getNotes() {
+        return this.api.get(this.path);
+    }
+
+    completeNote(note) {
+        return this.api.delete('${this.path}/${note.id}');
+    }
+
+}
